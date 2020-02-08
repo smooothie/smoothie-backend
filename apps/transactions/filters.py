4d5f729c294ab6e727770a777fec8cb71bc64b49
Valerii Kovalchuk <kovvalole@gmail.com>
2020-02-08 17:51:37 +0200

    Fix transactions filtering

--- apps/transactions/filters.py
+++ apps/transactions/filters.py
@@ -3,13 +3,11 @@ from django.db.models import Q
 import django_filters as filters
 from graphql_relay import from_global_id
 
-from apps.accounts.models import Account
 from apps.transactions.models import Transaction
 
 
 class TransactionFilter(filters.FilterSet):
-    account_from = filters.ModelChoiceFilter(queryset=Account.objects.all(),
-                                             method='account_transactions')
+    account_from = filters.CharFilter(method='account_transactions')
 
     class Meta:
         model = Transaction
@@ -17,6 +15,9 @@ class TransactionFilter(filters.FilterSet):
 
     def account_transactions(self, queryset, name, value):
         if value:
-            account_id = from_global_id(value)[1]
+            try:
+                account_id = from_global_id(value)[1]
+            except Exception:
+                return queryset.none()
             return queryset.filter(Q(account_from_id=account_id) | Q(account_to_id=account_id))
         return queryset
